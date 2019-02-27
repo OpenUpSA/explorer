21ed31428a546571e64ef61fd1c3c0e59e57b352
lunga <stumenz.complex@gmail.com>
2019-02-27 21:48:35 +0200

    begin dev for uploading csv

--- explorer/admin.py
+++ explorer/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.conf.urls import url
 
-# Register your models here.
+from .models import Dataset
+from .forms import ExplorerImportForm
+from .process import import_csv
+
+
+class ExplorerAdmin(admin.ModelAdmin):
+    change_list_template = 'explorer/explorer_changelist.djhtml'
+
+    def import_csv_dataset(self, request):
+        if request.method == 'POST' and request.is_ajax():
+            form = ExplorerImportForm(request.POST, request.FILES)
+            if form.is_valid():
+                name = form.cleaned_data['name']
+                csv_file = request.FILES['csv_file']
+                try:
+                    import_csv(name, csv_file)
+                    return JsonResponse({
+                        'status': 'ok',
+                    })
+                except Exception as error:
+                    return JsonResponse({
+                        'status': 'error',
+                        'form': str(error)
+                    })
+
+            else:
+                return JsonResponse({
+                    'status': 'error',
+                    'form': form.errors.as_json()
+                })
+
+        form = ExplorerImportForm()
+        return render(request, 'explorer/explorer_upload.djhtml',
+                      {'form': form})
+
+    def get_urls(self):
+        urls = super(ExplorerAdmin, self).get_urls()
+        custom_urls = [
+            url(r'^dataset-upload$',
+                self.admin_site.admin_view(self.import_csv_dataset),
+                name='upload')
+        ]
+        return custom_urls + urls
+
+
+admin.site.register(Dataset, ExplorerAdmin)
