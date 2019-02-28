58019677ce6a732a9af39b1997a06e75868d3e47
lunga <stumenz.complex@gmail.com>
2019-02-28 14:53:54 +0200

    fix packaging

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 setup(
     name='explorer',
     version='0.1',
-    packages=['explorer'],
+    packages=find_packages('explorer'),
     include_package_data=True,
     licence="MIT",
     description="Explorer is a simple Django app to view points on the map.",
