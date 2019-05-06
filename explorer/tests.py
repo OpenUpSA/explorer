e2a06a1ac22761cbace509eb0d306cdffbc28e4a
lunga <stumenz.complex@gmail.com>
2019-05-06 16:58:24 +0200

    add some tests

--- explorer/tests.py
+++ explorer/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.test import SimpleTestCase
 
-# Create your tests here.
+
+class ExplorerHomePageTest(SimpleTestCase):
+    def test_home_page(self):
+        """
+        request home page of explorer
+        """
+        response = self.client.get('/explorer', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_template(self):
+        """
+        Check that the correct template is used
+        """
+        response = self.client.get('/explorer', follow=True)
+        self.assertTemplateUsed(response, 'explorer/explorer.djhtml')
+
+    def test_home_page_content(self):
+        """
+        Check that the page has rendered correctly
+        """
+        response = self.client.get('/explorer', follow=True)
+        self.assertContains(response, '<div class="header item">Layers</div>')
