8f98dee0bfc643e284f501decdc4554fb3fa0642
lunga <lunga@complex-desktop>
2019-05-13 05:55:40 +0200

    need approval to show dataset and source of dataset

--- explorer/models.py
+++ explorer/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField, JSONField
+from django.contrib.auth.models import User
 
 
 class Dataset(models.Model):
     name = models.CharField(max_length=25)
     columns = JSONField()
     data = JSONField()
+    version = models.IntegerField(default=2018)
+    source = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    approved = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -15,6 +19,9 @@ class Geography(models.Model):
     name = models.CharField(max_length=25)
     columns = ArrayField(models.CharField(max_length=20))
     filename = models.FileField(upload_to='')
+    version = models.IntegerField(default=2018)
+    source = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    approved = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
