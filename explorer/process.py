a1383130063a6507459fb16a53ed14f83750b491
lunga <lunga@complex-desktop>
2019-05-13 05:57:46 +0200

    add additional fields

--- explorer/process.py
+++ explorer/process.py
@@ -4,7 +4,10 @@ from .models import Dataset
 import traceback
 
 
-def import_csv(dataset_name, csv_file):
+def import_csv(dataset_name, csv_file, version, user):
+    """
+    Import the csv data to json fields
+    """
     feature_collection = []
     try:
         frame = pandas.read_csv(csv_file)
@@ -13,6 +16,8 @@ def import_csv(dataset_name, csv_file):
         if 'Latitude' not in list(frame.columns) or 'Longitude' not in list(
                 frame.columns):
             raise Exception("Latitude/Longitude column not found in csv")
+        if 'Name' not in list(frame.columns):
+            raise Exception("Name column not found")
         for column in frame:
             if column not in ['Latitude', 'Longitude']:
                 columns[column] = list(frame[column].unique())
@@ -29,6 +34,8 @@ def import_csv(dataset_name, csv_file):
             feature = geojson.Feature(geometry=point, properties=properties)
             feature_collection.append(feature)
         Dataset.objects.create(
+            version=version,
+            source=user,
             name=dataset_name,
             columns=columns,
             data=geojson.FeatureCollection(feature_collection))
