e2354d9d49010d7833450f6ad74bc3a1e0b43ad1
lunga <lunga@complex-desktop>
2019-05-13 05:56:10 +0200

    add version to dataset

--- explorer/forms.py
+++ explorer/forms.py
@@ -4,3 +4,4 @@ from django import forms
 class ExplorerImportForm(forms.Form):
     name = forms.CharField(max_length=50, label="Dataset Name")
     csv_file = forms.FileField()
+    version = forms.IntegerField()
